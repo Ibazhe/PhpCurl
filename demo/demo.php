9fdc78ee2228f0576417f49b9fcf7d3728621cc4
bazhe <1031601644@qq.com>
2022-11-16 13:48:12 +0800

    屏蔽无用文件

--- demo/demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * author      : Administrator
- * creatTime   : 2022/11/15 22:40
- * description :
- */
-require "../vendor/autoload.php";
-$test = new \Ibazhe\PhpCurl\Curl();
-echo $test->open("GET","http://www.baidu.com")->send()->getResponseBody();
