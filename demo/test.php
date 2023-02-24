ab8161ae269af8af44d57f84e3d7431a0d8819f2
bazhe <1031601644@qq.com>
2023-02-24 22:45:15 +0800

    ssl 校验修复

--- demo/test.php
+++ demo/test.php
@@ -4,7 +4,15 @@
  * creatTime   : 2022/11/19 23:30
  * description :
  */
+var_dump(stripos('<!-- Copyright (C) Microsoft Corporation. All rights reserved. -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>正在重定向</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible','正在重定向'));
 
+exit();
 for($i=0;$i<10;$i++){
     echo $i;
     echo $i;
